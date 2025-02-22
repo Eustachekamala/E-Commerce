46b61b95471dbd33a7ae76f0420a15a570e228e0
eustachekamala <eustachejared25@gmail.com>
2025-02-22 16:48:50 +0300

    done with the configuration

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,23 @@
+import { Route, Routes } from "react-router-dom"
+import AuthLayout from "./components/auth/Layout"
+import AuthLogin from "./pages/auth/Login"
+import AuthRegister from "./pages/auth/Register"
 
 function App() {
  
 
   return (
-    <>
-      
-    </>
+    <div>
+      {/** common component */}
+  
+      <Routes>
+         <Route path="/auth" element={<AuthLayout/>}>
+            <Route path="login" element={<AuthLogin/>}/>
+            <Route path="register" element={<AuthRegister/>}/>
+         </Route>
+      </Routes>
+
+    </div>
   )
 }
 
