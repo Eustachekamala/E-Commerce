430e08a1047790ab3f7e444c106ea722a0b7ca28
eustachekamala <eustachejared25@gmail.com>
2025-02-22 22:55:11 +0300

    done with the form login and register

--- client/src/pages/auth/Login.jsx
+++ client/src/pages/auth/Login.jsx
@@ -1,7 +1,35 @@
+import CommonForm from "@/components/common/Form";
+import { loginFormControls } from "@/config";
+import { useState } from "react"
+import { Link } from "react-router-dom"
+
 
 const AuthLogin = () => {
+  const initialState = {
+    email : "",
+    password: ""
+  }
+
+  const [formData, setFormData ] = useState(initialState);
+
+  const onSubmit = () => {
+
+  }
+
   return (
-    <div>Login</div>
+    <div className="mx-auto w-full max-w-md space-y-6">
+      <div className="text-center">
+          <h1 className="text-3xl font-bold tracking-tight text-foreground">Sing in to your account</h1>
+          <p className="mt-2">Don&apos;t have an account<Link className="font-medium ml-2 text-primary hover:underline" to="/auth/register">Register</Link> </p>
+      </div>
+      <CommonForm
+        formControls={loginFormControls}
+        buttonText={"Sign In"}
+        formData={formData}
+        setFormData={setFormData}
+        onSubmit={onSubmit}
+      />
+    </div>
   )
 }
 
