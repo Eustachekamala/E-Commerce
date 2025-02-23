33826545d5703af1921abaac7ea891caac7ff4b1
eustachekamala <eustachejared25@gmail.com>
2025-02-23 22:11:37 +0300

    done with the authentification backend

--- server/routes/auth/authRoutes.mjs
+++ server/routes/auth/authRoutes.mjs
@@ -1,10 +1,19 @@
 import express from "express"
-import { registerUser, login, logout } from "../../controllers/auth/authController.mjs";
+import { registerUser, loginUser, logout, authMiddleware } from "../../controllers/auth/authController.mjs";
 
 const router = express.Router();
 
 router.route('/register').post(registerUser);
-router.route("/login").post(login);
+router.route("/login").post(loginUser);
 router.route("/logout").post(logout);
+router.route("/check-auth").get(authMiddleware, (req, res) => {
+  const user = req.user;
+  // Respond with the user data
+  res.status(200).json({
+    success: true,
+    message: "Authenticated user!",
+    user,
+  });
+});
 
 export default router;
